3a9e806cbd2bbf06287957c5a3f210402f1821b3
ewasion137 <formanban94@gmail.com>
2026-08-29 17:16:41 +0300

    discord rpc and track metadata 7
    Signed-off-by: ewasion137 <formanban94@gmail.com>

--- Zenith/script/preload.js
+++ Zenith/script/preload.js
@@ -5,14 +5,14 @@ contextBridge.exposeInMainWorld('electronAPI', {
     // files & folders
     selectFolder: (scanSubfolders) => ipcRenderer.invoke('dialog:openFolder', scanSubfolders),
     onReceiveTracks: (callback) => ipcRenderer.on('update-track-list', (event, tracks) => callback(tracks)),
-    
+
     // keys & shortcuts
     onGlobalCommand: (callback) => ipcRenderer.on('global-command', (event, cmd) => callback(cmd)),
-    
+
     // sound & metadata
     getAudioData: (filePath) => ipcRenderer.invoke('get-audio-data', filePath),
     getTrackMetadata: (trackPath) => ipcRenderer.invoke('get-track-metadata', trackPath),
-    
+
     // settings
     getTrackSettings: (trackPath) => ipcRenderer.invoke('get-track-settings', trackPath),
     saveTrackSettings: (data) => ipcRenderer.send('save-track-settings', data),
